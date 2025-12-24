23213ee9c606e2d105b185175b7f46749eba2353
NisenhouseTomax <dor.nisenhouse@tomax.io>
2025-12-24 13:04:44 +0200

    [TMVIXLT-9761] Hebrew translation

---
+++ lang/he/tomaetest.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_tomaetest
+ * @category    string
+ * @copyright   2024 Tomax ltd <roy@tomax.co.il>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Tomax ETest';
+
+$string['modulename'] = 'Tomax Activity';
+$string['modulenameplural'] = 'Tomax Activities';
+
+
+$string['tomaetestname'] = 'שם פעילות'; // activity name header in settings
+$string['pluginadministration'] = 'ניהול פעילות';
+
+$string['tetgeneralerror'] = 'שגיאה מ־TET';
+$string['tggeneralerror'] = 'שגיאה מ־TG';
+
+$string['activitystatus'] = 'סטטוס פעילות';
+$string['notreadyvalue'] = 'עדיין לא מוכן';
+$string['readyvalue'] = 'מוכן';
+$string['due'] = 'מועד אחרון';
+$string['notreadydescription'] = 'הפעילות עדיין בהכנה ואינה זמינה לסטודנטים.';
+$string['activitymanagementtitle'] = 'ניהול פעילות';
+$string['activitymanagementnotreadydescription'] = 'השתמשו במערכת Assessment Studio לעריכת הפעילות, הוספת שאלות והגדרת פורמט הפעילות.';
+$string['activitymanagementreadydescription'] = 'הפעילות מוכנה ורצה. ניתן לנטר את הפעילות בזמן אמת.';
+$string['openinassessmentstudio'] = 'פתחו ב־Assessment Studio';
+$string['importantnotestitle'] = 'הערות חשובות';
+$string['completebeforeaccess'] = 'יש להשלים את הגדרות הפעילות לפני שהסטודנטים יוכלו לגשת אליה.';
+$string['availableafterready'] = 'כאשר הפעילות תהיה מוכנה, היא תהפוך לזמינה לסטודנטים.';
+
+$string['readydescription'] = 'הפעילות מוכנה ורצה. ניתן לנטר את הפעילות בזמן אמת.';
+$string['activitynowavailable'] = 'הפעילות זמינה כעת לכל הסטודנטים.';
+$string['proctoringtoolsnote'] = 'אם כלי פרוקטורינג זמינים עבור פעילות זו, ניתן לנטר את פעילות הסטודנטים דרך מערכת הניטור של Tomax ב־Assessment Studio.';
+$string['checkinstarttime'] = 'זמן תחילת צ׳ק־אין';
+$string['actualstarttime'] = 'זמן התחלה בפועל';
+$string['dateclosed'] = 'מועד סגירה';
+$string['dateclosednote'] = 'הערה: זמן הסגירה אינו כולל התאמות זמן.';
+$string['activityfinished'] = 'הפעילות הסתיימה';
+$string['activityfinisheddescription'] = 'הפעילות הסתיימה ואינה זמינה עוד להגשה.';
+$string['activitynotready'] = 'הפעילות לא נמצאה';
+$string['activitynotreadydescription'] = 'המרצה עדיין מכין את הפעילות. כאשר היא תהיה מוכנה, תוכלו לצפות בה ולנטר את הסטודנטים במהלך הפעילות';
+$string['activitynotreadynote'] = '*באפשרותכם לבדוק מאוחר יותר אם הפעילות כבר מוכנה.';
+
+$string['activityready'] = 'הפעילות מוכנה!';
+$string['activityreadydescription'] = 'הפעילות זמינה לסטודנטים להתחיל.';
+$string['openmonitor'] = 'פתחו את הניטור';
+$string['noneditingnote1'] = 'כמורה ללא הרשאות עריכה תוכלו לנטר את פעילות הסטודנטים אך לא לערוך את הפעילות.';
+$string['noneditingnote2'] = 'תוכלו לראות התקדמות, זמן עבודה וסטטוס הגשות במערכת הניטור.';
+$string['noneditingnote3'] = 'אם אתם מבחינים בבעיה במהלך הפעילות, אנא צרו קשר עם מנהל הקורס.';
+$string['noneditingnote4'] = 'ההגשות של הסטודנטים ייאספו אוטומטית בסיום הפעילות.';
+
+$string['activitynotavailable'] = 'הפעילות עדיין לא זמינה';
+$string['activitynotavailabledescription'] = 'תוכלו לגשת לפעילות כאשר היא תהפוך לזמינה.';
+$string['availablefrom'] = 'הפעילות תהיה זמינה בתאריך';
+$string['downloadvixprompt'] = 'אנו ממליצים להוריד את תוכנת VIX לפני הפעילות, אם עדיין לא התקנתם אותה. התקנה מוקדמת תמנע תקלות של הרגע האחרון.';
+$string['downloadvix'] = 'הורדת VIX';
+
+$string['activityreadystudentdescription'] = 'הפעילות זמינה עבורכם. ניתן להוריד את הקבצים הנדרשים ולהתחיל.';
+$string['activityreadystep1title'] = 'הורידו את תוכנת VIX';
+$string['activityreadystep1subtitle'] = 'אם עדיין לא התקנתם את VIX, ניתן להוריד אותה בלחיצה על הכפתור למטה ולפעול לפי ההוראות.';
+$string['activityreadystep2title'] = 'פתחו את הפעילות ב־VIX';
+$string['activityreadystep2subtitle'] = 'לאחר התקנת VIX, לחצו על "פתחו את הפעילות ב־VIX" כדי לטעון את הפעילות.';
+$string['activityreadystep3title'] = 'השלימו והגישו את הפעילות';
+$string['activityreadystep3subtitle'] = 'עקבו אחרי ההנחיות ב־VIX כדי להשלים את הפעילות. ודאו שאתם מגישים לפני המועד.';
+$string['openinvix'] = 'פתחו את הפעילות ב־VIX';
