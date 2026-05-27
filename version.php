f223f53e9527d8405519ca4d0d55e68905963b14
Ron <ron.peker@tomax.io>
2026-05-27 15:40:28 +0300

    version

--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_tomaetest';
 $plugin->release = '0.1.0';
-$plugin->version = 2026052600;
+$plugin->version = 2026052700;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = array(
-    'local_tomax' => 2026052600
+    'local_tomax' => 2026052700
 );
